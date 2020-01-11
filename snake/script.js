0546921b609eeba4a8d1a49a323b376c4314523c
Tom <tomwilliamfranklin@gmail.com>
2020-01-11 21:33:55 +0000

    Fixed issues with game not ending. snake going back on itself.

--- snake/script.js
+++ snake/script.js
@@ -2,17 +2,26 @@ const w = 1200;
 const h = 1200;
 const square = 100;
 const map = new Array(w/square);
+const foodmap = new Array(w/square);
 const background = [38,38,38];
+const endBackground = [18, 18, 18];
 const player = [201, 111, 66];
 const playerStroke = [81, 92, 83];
 const head = [250, 138, 82];
 const dead = [166, 45, 45];
+let currentHead = [0,0];
+let snakeLength = 0;
+let gamePlaying = true;
+
 let direction = "up";
 let sketch = function(p) {
     for(var i = 0; i<map.length; i++) {
         map[i] = new Array(h/square);
+        foodmap[i] = new Array(h/square);
     }
+
     p.setup = function() {
+        snakeLength = 0;
         direction = "up";
         const wrandom = Math.floor(Math.random() * (w/square));
         const hrandom = Math.floor(Math.random() * (h/square));
@@ -23,57 +32,55 @@ let sketch = function(p) {
         for(var i = 0; i!=w; i = i+square) {
             for(var ii = 0; ii!=h; ii = ii+square) {
                 map[i/square][ii/square] = false;
+                foodmap[i/square][ii/square] = false;
                 p.square(i, ii, square);
             }
         }
         p.fill(player);
         p.square(w/2,h/2, square);
-        currentSpot[0] = w/2/square;
-        currentSpot[1] = h/2/square;
+        currentHead[0] = w/2/square;
+        currentHead[1] = h/2/square;
+
         map[w/square/2][h/square/2] = true;
-        p.frameRate(5);
+        snakeLength = 1;
+
+        p.frameRate(10);
+        createFood();
     }
 
     const coor = {up:[0, -1],  //im actually a genius
     left:[ -1, 0],          right:[ +1, 0],
                 down:[0, +1]};
 
-    let currentSpot = [0,0];
-
+    const snake = [];
+    let previousDirection = "down";
     p.draw = function() { 
         const toHead = [];
+        var temp1 = currentHead[0];
+        var temp2 = currentHead[1];
+                if(map[temp1 + coor[direction][0]] != null) {
+                    if(map[temp1 + coor[direction][0]][temp2+coor[direction][1]] != null) {
+                        if(map[temp1 + coor[direction][0]][temp2+coor[direction][1]] == true) {
+                            endGame();
+                        } else {
+                            if(foodmap[temp1 + coor[direction][0]][temp2+coor[direction][1]] === true) {
+                                snakeLength++;
+                                foodmap[temp1 + coor[direction][0]][temp2+coor[direction][1]] = false;
+                                createFood();
+                            }
+                            makeHead(temp1 + coor[direction][0], temp2 + coor[direction][1]);
+                            addToSnake(temp1,temp2);
 
-        nested();
-        function nested() {
-                    var temp1 = currentSpot[0];
-                    var temp2 = currentSpot[1];
-                            if(map[temp1 + coor[direction][0]] != null) {
-                                if(map[temp1 + coor[direction][0]][temp2+coor[direction][1]] != null) {
-                                    if(map[temp1 + coor[direction][0]][temp2+coor[direction][1]] == true) {
-                                        endGame();
-                                    } else {
-                                        makeHead(temp1 + coor[direction][0], temp2 + coor[direction][1]);
-                                        p.fill(player);
-                                        p.square(temp1*square,temp2*square, square);
-                                        return;
-                                        
-                                    }
-                                } else {
-                                    endGame();
-                            }    
-                            }  else {
-                                endGame();
+                            if(snake.length > snakeLength) {
+                                removeTail();
                             }                
-                    // for(let j = 0; j!=coor.length; j++) { 
-                    //     if(map[temp1 + coor[j][0]] != null) {
-                    //         if(map[temp1 + coor[j][0]][temp2 + coor[j][1]] != null) {
-                    //             toLive.push([temp1 + coor[j][0], temp2 + coor[j][1]])
-                    //         } 
-                    //     }
-                    // }
-        }
-
-
+                        }
+                    } else {
+                        endGame();
+                }    
+                }  else {
+                    endGame();
+                }                
         p.updatePixels();
     }
 
@@ -87,46 +94,89 @@ let sketch = function(p) {
                         p.fill(dead);
                         p.square(i,ii, square);
                     }         
-                
+                    else {
+                        p.fill(endBackground);
+                        p.square(i,ii,square);
+                    }
             }
         }
 
         p.updatePixels();
+        gamePlaying = false;
+        p.frameRate(0);
+        
+    }
+    previousDirection = "up";
+     p.keyPressed = function() {
+            if(!gamePlaying) {
+                p.setup();  
+                gamePlaying = true; 
+            } else {
+            previousDirection = direction;
+
+            if(p.keyCode === p.UP_ARROW) {
+                if(previousDirection != "down") {
+                    direction = "up";
+                    console.log(direction);
+                }
+            }
+            if(p.keyCode === p.LEFT_ARROW) {
+                if(previousDirection != "right") {
+                    direction = "left";
+                    console.log(direction);
+                }
+            }
+            if(p.keyCode === p.RIGHT_ARROW) {
+                if(previousDirection != "left") {
+                    direction = "right";
+                    console.log(direction);
+                }
+            }
+            if(p.keyCode === p.DOWN_ARROW) {
+                if(previousDirection != "up") {
+                    direction = "down";
+                    console.log(direction);
+                }
+            }
+        }
     }
 
-     p.keyPressed = function() {
-        if(p.keyCode === p.UP_ARROW) {
-            direction = "up";
-            console.log(direction);
-        }
-        if(p.keyCode === p.LEFT_ARROW) {
-            direction = "left";
-            console.log(direction);
-        }
-        if(p.keyCode === p.RIGHT_ARROW) {
-            direction = "right";
-            console.log(direction);
-        }
-        if(p.keyCode === p.DOWN_ARROW) {
-            direction = "down";
-            console.log(direction);
-        }
+    function addToSnake(width,height) {
+        p.fill(player);
+        p.square(width*square,height*square, square);
+        snake.push([width, height]);
     }
 
     function makeHead(width,height) {
+      //  snake.unshift([width, height]);
         p.fill(head);
         p.square(width*square,height*square, square);
         map[width][height] = true;
-        currentSpot[0] = width;
-        currentSpot[1] = height;
+        currentHead[0] = width;
+        currentHead[1] = height;
     }
 
-    function randomSquare() {
+    function removeTail() {
+        p.fill(background);
+        var tail = snake.shift();
+        map[tail[0]][tail[1]] = false;
+        p.square(tail[0]*square,tail[1]*square, square);
+    }
+
+    function createFood() {
         const r = Math.floor(Math.random() * 255);
         const g = Math.floor(Math.random() * 255);
         const b = Math.floor(Math.random() * 255);
         p.fill(r, g, b);
-        p.square(i, ii, square);
+
+        const wRandom = Math.floor(Math.random() * w/square) * square;
+        const hRandom = Math.floor(Math.random() * h/square) * square;
+        if(map[wRandom/square][hRandom/square]) {
+            createFood();
+        } else {
+            foodmap[wRandom/square][hRandom/square] = true;
+            p.square(wRandom, hRandom, square);
+        }
     }
 }
 
